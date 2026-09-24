4958e221e89d6592902336c5a54277c5f6069c0f
bagir_yv81 <bagir_yv81@STHQ012D-11>
2026-09-24 21:20:51 +0400

    a

--- LESSON_1_Task/Program.cs
+++ LESSON_1_Task/Program.cs
@@ -1,58 +1,96 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Xml;
 public class program
 {
+    public static void Create1()
+    {
+        try
+        {
+            Console.Write("Start Process name: ");
+            var choice2 = Console.ReadLine();
+            var datas = Process.Start(choice2);
+            // var datas = Process.Start("calc"); Misal olaraq 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex}");
+        }
+    }
+    //----------------------------------
+    public static void Delete()
+    {
+        try
+        {
+            Console.Write("Delete Process name: ");
+            int choise3 = Convert.ToInt32(Console.ReadLine());
+            var data = Process.GetProcessById(choise3);
+            data.Kill();
+            data.WaitForExit();
+            data.Dispose();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex}");
+        }
+    }
+
     public static void Main(string[] args)
     {
         // dersde yazdim codeni windows from acilmadi(admin parol isteyirdi) ona gorem app da yazdim 
         Thread.CurrentThread.Name = "Task_Lesson_1";
         Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} ");
-        //Console.WriteLine(Thread.CurrentThread.Name);
-        Process[] systemProcesses = Process.GetProcessesByName("");
-        if (systemProcesses.Length > 0)
+        Process[] systemProcesses = Process.GetProcesses();
+        //if (systemProcesses.Length > 0)
+        //{
+        //    Process Proc = systemProcesses[0];
+        //    Console.WriteLine($"Process Name: {Proc.ProcessName}");
+        //    Console.WriteLine($"Process ID : {Proc.Id}");
+        //    Console.WriteLine($"Handle Count: {Proc.HandleCount}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Error: System NOt founded !!!!");
+        //}
+
+        foreach (Process p in systemProcesses)
         {
-            Process Proc = systemProcesses[0];
-            Console.WriteLine($"Process Name: {Proc.ProcessName}");
-            Console.WriteLine($"Process ID : {Proc.Id}");
-            Console.WriteLine($"Handle Count: {Proc.HandleCount}");
+            Console.WriteLine($"Name: {p.ProcessName}");
+            Console.WriteLine($"Id: {p.Id}");
+            Console.WriteLine($"Handle Count: {p.HandleCount}");
+            Console.WriteLine($"Thread Count: {p.Threads.Count}");
+            Console.WriteLine($"Machine Name: {p.MachineName}");
         }
-        else
+
+        //Thread t = new Thread(() =>
+        //{
+        //    Console.WriteLine("System islyir .");
+        //});
+        //t.Start();
+        //Process Proces1 = Process.GetCurrentProcess();
+        //int threadCount = Proces1.Threads.Count;
+        //Console.WriteLine($"Proces name : {Proces1.ProcessName}");
+        //Console.WriteLine($"Thread count : {threadCount}");
+        //t.Join();
+        //0000000000000000000000000000000000000000000000    
+        while (true)
         {
-            Console.WriteLine("Error: System NOt founded !!!!");
+            Console.Write("\n1.Add\n2.Delete\nSecim: ");
+            int choise1 = Convert.ToInt32(Console.ReadLine());
+            if (choise1 == 1)
+            {
+                Create1();
+            }
+            else if (choise1 == 2)
+            {
+                Delete();
+            }
+            else
+            {
+                Console.WriteLine("Error: Secim duzgun daxil edin !!!");
+            }
         }
 
-        Thread t = new Thread(() =>
-         {
-             Console.WriteLine("System islyir ."); 
-         });
-        t.Start();
-        Process Proces1 = Process.GetCurrentProcess();
-        int threadCount = Proces1.Threads.Count;
-        Console.WriteLine($"Proces name : {Proces1.ProcessName}");
-        Console.WriteLine($"Thread count : {threadCount}");
-        t.Join();
 
-
-        Console.Write("\n1.Add\n2.Delete\nSecim: ");
-        int choise = Convert.ToInt32( Console.ReadLine() );
-
-        if (choise == 1)
-        {
-            //Console.WriteLine(t.ManagedThreadId); // Yeni 1 thread yarandi
-
-            Console.Write("Start Process name: ");
-            var choice = Console.ReadLine();
-            var datas = Process.Start(choice);
-            // var datas = Process.Start("calc"); Misal olaraq 
-        }
-        else if (choise == 2)
-        {
-            var data = Process.GetProcessById(choise);
-            data.Kill();
-            data.WaitForExit();
-            data.Dispose();
-        }
 
     }
 }
