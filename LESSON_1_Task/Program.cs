21caed2861ef9bf7c7fb7fa437319929d744f242
Yusuf-018-MRX <yusufbagirov2010@gmail.com>
2026-09-24 20:58:25 +0400

    Enhance process creation and deletion functionality
    Refactor process management methods and improve user interaction.

--- LESSON_1_Task/Program.cs
+++ LESSON_1_Task/Program.cs
@@ -1,8 +1,41 @@
-﻿using System;
+using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Xml;
 public class program
 {
+    public static void Create1()
+    {
+        try
+        {
+            Console.Write("Start Process name: ");
+            var choice2 = Console.ReadLine();
+            var datas = Process.Start(choice2);
+            // var datas = Process.Start("calc"); Misal olaraq 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex}");
+        }
+    }
+    //----------------------------------
+    public static void Delete()
+    {
+        try
+        {
+            Console.Write("Delete Process name: ");
+            int choise3 = Convert.ToInt32(Console.ReadLine());
+            var data = Process.GetProcessById(choise3);
+            data.Kill();
+            data.WaitForExit();
+            data.Dispose();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex}");
+        }
+    }
+
     public static void Main(string[] args)
     {
         // dersde yazdim codeni windows from acilmadi(admin parol isteyirdi) ona gorem app da yazdim 
@@ -23,36 +56,35 @@ public class program
         }
 
         Thread t = new Thread(() =>
-         {
-             Console.WriteLine("System islyir ."); 
-         });
+        {
+            Console.WriteLine("System islyir .");
+        });
         t.Start();
         Process Proces1 = Process.GetCurrentProcess();
         int threadCount = Proces1.Threads.Count;
         Console.WriteLine($"Proces name : {Proces1.ProcessName}");
         Console.WriteLine($"Thread count : {threadCount}");
         t.Join();
-
-
-        Console.Write("\n1.Add\n2.Delete\nSecim: ");
-        int choise = Convert.ToInt32( Console.ReadLine() );
-
-        if (choise == 1)
+        //0000000000000000000000000000000000000000000000    
+        while (true)
         {
-            //Console.WriteLine(t.ManagedThreadId); // Yeni 1 thread yarandi
+            Console.Write("\n1.Add\n2.Delete\nSecim: ");
+            int choise1 = Convert.ToInt32(Console.ReadLine());
+            if (choise1 == 1)
+            {
+                Create1();
+            }
+            else if (choise1 == 2)
+            {
+                Delete();
+            }
+            else
+            {
+                Console.WriteLine("Error: Secim duzgun daxil edin !!!");
+            }
+        }
 
-            Console.Write("Start Process name: ");
-            var choice = Console.ReadLine();
-            var datas = Process.Start(choice);
-            // var datas = Process.Start("calc"); Misal olaraq 
-        }
-        else if (choise == 2)
-        {
-            var data = Process.GetProcessById(choise);
-            data.Kill();
-            data.WaitForExit();
-            data.Dispose();
-        }
+
 
     }
 }
